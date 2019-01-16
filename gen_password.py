822c2f165a77732614068a6b251dc8c4a71332e5
wascha <wascha@ukr.net>
2019-01-16 20:45:04 +0200

    test

--- gen_password.py
+++ gen_password.py
@@ -1,7 +1,8 @@
 from string import ascii_letters, ascii_lowercase, ascii_uppercase, digits
 from random import choice
 chars = ascii_letters + digits
+
 def pd(n):
 	return ''.join(choice(chars) for _ in range(n))
 
-print(pd(8))
+print(pd(10))
